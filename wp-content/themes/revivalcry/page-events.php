07e7a1130d7cd8a453f829be317f83ad1d42b7d9
Jesse Kade <jessekade@gmail.com>
2011-12-31 05:56:48 -0800

    working on single event page

--- wp-content/themes/revivalcry/page-events.php
+++ wp-content/themes/revivalcry/page-events.php
@@ -7,15 +7,10 @@ get_header(); ?>
 		<div id="content-container">
 			<section id="content" role="main">
 
-			<?php get_template_part( 'loop', 'page' ); ?>
-			
-			
+	<?php get_template_part( 'loop', 'page' ); ?>
 
 			
 			</section><!-- #content -->
-			
-			
-			
 		</div><!-- #content-container -->
 
 <?php get_footer(); ?>
