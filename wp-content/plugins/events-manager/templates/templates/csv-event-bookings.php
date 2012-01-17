36db9298f38e3fb1967f580012aad780c72ddf6d
root <root@li330-58.members.linode.com>
2012-01-17 08:42:48 -0800

    updated events manager

--- wp-content/plugins/events-manager/templates/templates/csv-event-bookings.php
+++ wp-content/plugins/events-manager/templates/templates/csv-event-bookings.php
@@ -14,7 +14,7 @@ $labels = apply_filters('em_csv_bookings_headers',array(
 	'Comment'
 ));
 $file = sprintf(__('Booking details for "%s" as of %s','dbem'),$EM_Event->name, date_i18n('D d M Y h:i', current_time('timestamp'))) .  "\n";
-$file = '"'. implode('","', $labels). '"' .  "\n";
+$file .= '"'. implode('","', $labels). '"' .  "\n";
 
 //Rows
 foreach( $EM_Event->get_bookings()->bookings as $EM_Booking ) {
@@ -23,12 +23,13 @@ foreach( $EM_Event->get_bookings()->bookings as $EM_Booking ) {
 		/* @var $EM_Ticket EM_Ticket */
 		/* @var $EM_Ticket_Booking EM_Ticket_Booking */
 		$EM_Ticket = $EM_Ticket_Booking->get_ticket();
+		$row_output = '';
 		$row = array(
 			$EM_Booking->booking_id,
 			$EM_Booking->person->get_name(),
 			$EM_Booking->person->user_email,
 			$EM_Booking->person->phone,
-			date('Y-m-d h:i', $EM_Booking->timestamp),
+			date('Y-m-d H:i', $EM_Booking->timestamp),
 			$EM_Booking->get_status(),
 			$EM_Ticket->name,
 			$EM_Ticket_Booking->get_spaces(),
@@ -39,10 +40,10 @@ foreach( $EM_Event->get_bookings()->bookings as $EM_Booking ) {
 		foreach($row as $value){
 			$value = str_replace('"', '""', $value);
 			$value = str_replace("=", "", $value);
-			$file .= '"' .  preg_replace("/\n\r|\r\n|\n|\r/", ".     ", $value) . '",';
+			$row_output .= '"' .  preg_replace("/\n\r|\r\n|\n|\r/", ".     ", $value) . '",';
 		}
-		$file = apply_filters('em_csv_bookings_loop_after', $file, $EM_Ticket_Booking, $EM_Booking);
-		$file .= "\n";
+		$row_output = apply_filters('em_csv_bookings_loop_after', $row_output, $EM_Ticket_Booking, $EM_Booking);
+		$file .= $row_output."\n";
 	}
 }
 
