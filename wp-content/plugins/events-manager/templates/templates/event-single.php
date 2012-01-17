ee4bd6ef9ca1d5b28e9123cb2178960f6030c30c
Jesse Kade <jessekade@gmail.com>
2012-01-17 08:57:56 -0800

    fixed merge conflict

--- wp-content/plugins/events-manager/templates/templates/event-single.php
+++ wp-content/plugins/events-manager/templates/templates/event-single.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 /*
  * This page displays a single event, called during the em_content() if this is an event page.
@@ -10,17 +9,4 @@
 global $EM_Event;
 /* @var $EM_Event EM_Event */
 echo $EM_Event->output_single();
-=======
-<?php
-/*
- * This page displays a single event, called during the em_content() if this is an event page.
- * You can override the default display settings pages by copying this file to yourthemefolder/plugins/events-manager/templates/ and modifying it however you need.
- * You can display events however you wish, there are a few variables made available to you:
- * 
- * $args - the args passed onto EM_Events::output() 
- */
-global $EM_Event;
-/* @var $EM_Event EM_Event */
-echo $EM_Event->output_single();
->>>>>>> 36db9298f38e3fb1967f580012aad780c72ddf6d
 ?>
