d739e479359492620ffbbaab0cfe5f5e53f34518
sharpmachine <info@sharpmachinemedia.com>
2015-11-16 10:24:15 -0800

    Site maintenance for Nov 16, 2015

--- wp-content/w3tc-config/master.php
+++ wp-content/w3tc-config/master.php
@@ -408,7 +408,7 @@ return array(
 	'browsercache.other.etag' => false,
 	'browsercache.other.w3tc' => true,
 	'browsercache.other.replace' => false,
-	'browsercache.timestamp' => '1441642984',
+	'browsercache.timestamp' => '1445268422',
 	'mobile.enabled' => true,
 	'mobile.rgroups' => array(
 		'high' => array(
